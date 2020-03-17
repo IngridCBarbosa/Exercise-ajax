f598da38eae6fea9174e68ab9dc029275da1f852
INGRID <ingridcarolinelima@hotmail.com>
2020-03-17 12:22:36 -0300

    update

--- server.js
+++ server.js
@@ -6,6 +6,29 @@ app.use(express.static('.'))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destino : function (req,file,callback){
+        callback(null,'.')
+    },
+
+    filename:function(req,file,callback){
+        callback(null,`${Date.now}_${file.originalname}`)
+    }
+})
+
+const upload = multer({storage}).single('arquivo')
+
+app.post('/upload',(req,res) =>{
+    upload(req,res,err => {
+        if(err){
+            return res.end('Ocorreu um erro.')
+        }
+        res.end('Concluído com sucesso')
+    })
+})
+
 app.get('/teste',(req,res) => res.send('ok'))
 
 app.listen(8081, () => console.log('Executando'))
