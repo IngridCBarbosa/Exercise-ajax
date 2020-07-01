b4be3fd3a0f199f5ef44715ac7a27a64cb64531d
IngridCBarbosa <ingridcarolinelima@hotmail.com>
2020-07-01 15:32:36 -0300

    update XMLHttpResquest 2

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const multer = require('multer');
+const { request, response } = require('express');
 
 const app = express();
 
@@ -11,5 +13,27 @@ app.use(bodyParser.json());
 app.get('/teste', (request, response) => {
     return response.send('OK')
 });
+
+const storage = multer.diskStorage({
+    destination: function (request, file, callback){
+        callback(null,'./upload');
+    },
+    filename: function(request, file, callback){
+        callback(null, `${Date.now()}_${file.originalname}`);
+    }
+});
+
+const upload = multer({storage}).single('arquivo');
+
+
+app.post('/upload',(request,response) => {
+    upload(request,response,err =>{
+        if(err){
+            return response.end('Ocorreu um erro.');
+        }
+        response.end('Concluído com sucesso');
+    })
+})
+
 
 app.listen(3333, () => console.log('Executando'));
